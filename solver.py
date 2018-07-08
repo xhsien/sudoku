e0eb7f431dbee5b4b962938842bc0ca8a16bd24f
Christopher Boo <christopherboo66@gmail.com>
2018-07-08 10:51:05 +0800

    initial commit

---
+++ solver.py
@@ -0,0 +1,264 @@
+import tkinter as tk
+import itertools
+
+class Solver:
+
+    def start(self):
+        self.puzzle = []
+        for i in range(9):
+            self.puzzle.append([])
+            for j in range(9):
+                self.puzzle[i].append([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    def solve(self):
+        progress = True
+        while progress:
+            progress = False
+            progress = progress or self.redundantChoiceRemoval()
+            progress = progress or self.onlyChoiceRule()
+            progress = progress or self.subgroupElimination()
+            progress = progress or self.hiddenTwinExclusion()
+            progress = progress or self.nakedTwinExclusion()
+
+    def redundantChoiceRemoval(self):
+        worked = False
+        for i, j in itertools.product(range(9), repeat=2):
+            if len(self.puzzle[i][j]) == 1:
+                c = self.puzzle[i][j][0]
+                # row
+                for k in range(9):
+                    if k == j:
+                        continue
+                    if c in self.puzzle[i][k]:
+                        self.puzzle[i][k].remove(c)
+                        if len(self.puzzle[i][k]) == 1:
+                            self.puzzle[i][k] = [self.puzzle[i][k][0]]
+                            worked = True
+                # col
+                for k in range(9):
+                    if k == i:
+                        continue
+                    if c in self.puzzle[k][j]:
+                        self.puzzle[k][j].remove(c)
+                        if len(self.puzzle[k][j]) == 1:
+                            self.puzzle[k][j] = [self.puzzle[k][j][0]]
+                            worked = True
+
+                # box
+                p = (i // 3) * 3
+                q = (j // 3) * 3
+                for kp in range(p, p + 3):
+                    for kq in range(q, q + 3):
+                        if kp == i and kq == j:
+                            continue
+                        if c in self.puzzle[kp][kq]:
+                            self.puzzle[kp][kq].remove(c)
+                            if len(self.puzzle[kp][kq]) == 1:
+                                self.puzzle[kp][kq] = [self.puzzle[kp][kq][0]]
+                                worked = True
+        return worked
+
+    def onlyChoiceRule(self):
+        worked = False
+        for i, j in itertools.product(range(9), repeat=2):
+            if len(self.puzzle[i][j]) == 1:
+                continue
+            for c in self.puzzle[i][j]:
+                isOnlyChoice = True
+                # row
+                for k in range(9):
+                    if k == j:
+                        continue
+                    if c in self.puzzle[i][k]:
+                        isOnlyChoice = False
+                        break
+                if isOnlyChoice:
+                    self.puzzle[i][j] = [c]
+                    worked = True
+                    break
+                
+                isOnlyChoice = True
+                for k in range(9):
+                    if k == i:
+                        continue
+                    if c in self.puzzle[k][j]:
+                        isOnlyChoice = False
+                        break
+                if isOnlyChoice:
+                    self.puzzle[i][j] = [c]
+                    worked = True
+                    break
+
+                isOnlyChoice = True
+                p = (i // 3) * 3
+                q = (j // 3) * 3
+                for kp in range(p, p + 3):
+                    for kq in range(q, q + 3):
+                        if kp == i and kq == j:
+                            continue
+                        if c in self.puzzle[kp][kq]:
+                            isOnlyChoice = False
+                            break
+                if isOnlyChoice:
+                    self.puzzle[i][j] = [c]
+                    worked = True
+                    break
+        return worked
+
+    def subgroupElimination(self):
+        worked = False
+        for c in range(1, 10):
+            for gi in range(3):
+                for gj in range(3):
+                    row = [0, 0, 0]
+                    col = [0, 0, 0]
+                    for i in range(gi * 3, gi * 3 + 3):
+                        for j in range(gj * 3, gj * 3 + 3):
+                            if c in self.puzzle[i][j]:
+                                row[i % 3] = 1
+                                col[j % 3] = 1
+                    if sum(row) == 1:
+                        k = 0
+                        for x in range(3):
+                            if row[x] == 1:
+                                k = x
+                        for j in range(9):
+                            if j in range(gj * 3, gj * 3 + 3):
+                                continue
+                            if c in self.puzzle[gi * 3 + k][j]:
+                                self.puzzle[gi * 3 + k][j].remove(c)
+                                if len(self.puzzle[gi * 3 + k][j]) == 1:
+                                    self.puzzle[gi * 3 + k][j] = [self.puzzle[gi * 3 + k][j][0]]
+                                worked = True
+                    if sum(col) == 1:
+                        k = 0
+                        for x in range(3):
+                            if col[x] == 1:
+                                k = x
+                        for i in range(9):
+                            if i in range(gi * 3, gi * 3 + 3):
+                                continue
+                            if c in self.puzzle[i][gj * 3 + k]:
+                                self.puzzle[i][gj * 3 + k].remove(c)
+                                if len(self.puzzle[i][gj * 3 + k]) == 1:
+                                    self.puzzle[i][gj * 3 + k] = [self.puzzle[i][gj * 3 + k][0]]
+                                worked = True
+        return worked
+
+    def hiddenTwinExclusion(self):
+        worked = False
+        for c1 in range(1, 10):
+            for c2 in range(c1 + 1, 10):
+                # row
+                for i in range(9):
+                    cnt = 0
+                    for j in range(9):
+                        if c1 in self.puzzle[i][j] and c2 in self.puzzle[i][j]:
+                            cnt += 1
+                        elif c1 in self.puzzle[i][j] or c2 in self.puzzle[i][j]:
+                            cnt = -1
+                            break
+                    
+                    if cnt != 2:
+                        continue
+
+                    for j in range(9):
+                        if c1 in self.puzzle[i][j] and c2 in self.puzzle[i][j]:
+                            if len(self.puzzle[i][j]) > 2:
+                                worked = True
+                                self.puzzle[i][j] = [c1, c2]
+
+                # col
+                for j in range(9):
+                    cnt = 0
+                    for i in range(9):
+                        if c1 in self.puzzle[i][j] and c2 in self.puzzle[i][j]:
+                            cnt += 1
+                        elif c1 in self.puzzle[i][j] or c2 in self.puzzle[i][j]:
+                            cnt = -1
+                            break
+
+                    if cnt != 2:
+                        continue
+
+                    for i in range(9):
+                        if c1 in self.puzzle[i][j] and c2 in self.puzzle[i][j]:
+                            if len(self.puzzle[i][j]) > 2:
+                                worked = True
+                                self.puzzle[i][j] = [c1, c2]
+
+                #box
+                for gi in range(3):
+                    for gj in range(3):
+                        cnt = 0
+                        for i in range(gi * 3, gi * 3 + 3):
+                            if cnt == -1:
+                                break
+                            for j in range(gj * 3, gj * 3 + 3):
+                                if c1 in self.puzzle[i][j] and c2 in self.puzzle[i][j]:
+                                    cnt += 1
+                                elif c1 in self.puzzle[i][j] or c2 in self.puzzle[i][j]:
+                                    cnt = -1
+                                    break
+
+                        if cnt != 2:
+                            continue
+                        # print(c1, c2, gi, gj)
+                        return False
+                        for i in range(gi * 3, gi * 3 + 3):
+                            for j in range(gj * 3, gj * 3 + 3):
+                                if c1 in self.puzzle[i][j] and c2 in self.puzzle[i][j]:
+                                    if len(self.puzzle[i][j]) > 2:
+                                        worked = True
+                                        self.puzzle[i][j] = [c1, c2]
+        return worked
+
+
+    def nakedTwinExclusion(self):
+        worked = False
+        for i, j in itertools.product(range(9), repeat=2):
+            if len(self.puzzle[i][j]) != 2:
+                continue
+            
+            c1 = self.puzzle[i][j][0]
+            c2 = self.puzzle[i][j][1]
+
+            # row
+            for k in range(9):
+                if k == j:
+                    continue
+                if self.puzzle[i][k] == self.puzzle[i][j]:
+                    for x in range(9):
+                        if x == j or x == k:
+                            continue
+                        if c1 in self.puzzle[i][x]:
+                            worked = True
+                            self.puzzle[i][x].remove(c1)
+                            if len(self.puzzle[i][x]) == 1:
+                                self.puzzle[i][x] = [self.puzzle[i][x][0]]
+                        if c2 in self.puzzle[i][x]:
+                            worked = True
+                            self.puzzle[i][x].remove(c2)
+                            if len(self.puzzle[i][x]) == 1:
+                                self.puzzle[i][x] = [self.puzzle[i][x][0]]
+                        
+            # col
+            for k in range(9):
+                if k == i:
+                    continue
+                if self.puzzle[k][j] == self.puzzle[i][j]:
+                    for x in range(9):
+                        if x == i or x == k:
+                            continue
+                        if c1 in self.puzzle[x][j]:
+                            worked = True
+                            self.puzzle[x][j].remove(c1)
+                            if len(self.puzzle[x][j]) == 1:
+                                self.puzzle[x][j] = [self.puzzle[x][j][0]]
+                        if c2 in self.puzzle[x][j]:
+                            worked = True
+                            self.puzzle[x][j].remove(c2)
+                            if len(self.puzzle[x][j]) == 1:
+                                self.puzzle[x][j] = [self.puzzle[x][j][0]]
+                            
+        return worked
---
+++ ui.py
@@ -0,0 +1,116 @@
+import tkinter as tk
+
+from solver import Solver
+
+# WINDOW PARAMETER
+
+MARGIN = 20
+SIDE   = 50
+WIDTH  = HEIGHT = MARGIN * 2 + SIDE * 9
+
+class Ui(tk.Frame):
+    """
+    Graphical user interface for accepting sudoku input
+    """
+    def __init__(self, master, game):
+        self.master = master
+        tk.Frame.__init__(self, master)
+        
+        self.game = game
+        self.row, self.col = -1, -1
+
+        self.__initUI()
+
+    def __initUI(self):
+        self.pack(fill="both", expand=1)
+
+        self.canvas = tk.Canvas(self, width=WIDTH, height=HEIGHT)
+        self.canvas.pack(fill="both", side="top")
+
+        solveButton = tk.Button(self, text="Solve", command=self.__solve)
+        solveButton.pack(fill="x", padx="10", pady="5")
+
+        clearButton = tk.Button(self, text="Clear", command=self.__clear)
+        clearButton.pack(fill="x", padx="10", pady="5")
+
+        self.__draw_grid()
+
+        self.canvas.bind("<Button-1>", self.__cell_clicked)
+        self.canvas.bind("<Key>", self.__key_pressed)
+
+    def __draw_grid(self):
+        for i in range(10):
+            color = "blue" if i % 3 == 0 else "gray"
+
+            x0 = MARGIN + i * SIDE
+            y0 = MARGIN
+            x1 = MARGIN + i * SIDE
+            y1 = HEIGHT - MARGIN
+            self.canvas.create_line(x0, y0, x1, y1, fill=color)
+
+            x0 = MARGIN
+            y0 = MARGIN + i * SIDE
+            x1 = WIDTH - MARGIN
+            y1 = MARGIN + i * SIDE
+            self.canvas.create_line(x0, y0, x1, y1, fill=color)
+
+    def __draw_cursor(self):
+        self.canvas.delete("cursor")
+        if self.row >= 0 and self.col >= 0:
+            x0 = MARGIN + self.col * SIDE + 1
+            y0 = MARGIN + self.row * SIDE + 1
+            x1 = MARGIN + (self.col + 1) * SIDE + 1
+            y1 = MARGIN + (self.row + 1) * SIDE + 1
+            self.canvas.create_rectangle(x0, y0, x1, y1, outline="red", tags="cursor")
+
+    def __cell_clicked(self, event):
+        x, y = event.x, event.y
+        if MARGIN < x < WIDTH - MARGIN and MARGIN < y < HEIGHT - MARGIN:
+            self.canvas.focus_set()
+
+            row, col = (y - MARGIN) // SIDE, (x - MARGIN) // SIDE
+
+            if (row, col) == (self.row, self.col):
+                self.row, self.col = -1, -1
+
+            self.row, self.col = row, col
+
+        self.__draw_cursor()
+
+    def __key_pressed(self, event):
+        if self.row >= 0 and self.col >= 0 and event.char in "123456789":
+            self.game.puzzle[self.row][self.col] = [int(event.char)]
+            self.__draw_puzzle()
+            self.__draw_cursor()
+            self.col, self.row = -1, -1
+
+    def __clear(self):
+        self.game.start()
+        self.__draw_puzzle()
+
+    def __solve(self):
+        self.game.solve()
+        self.__draw_puzzle()
+
+    def __draw_puzzle(self):
+        self.canvas.delete("numbers")
+        for i in range(9):
+            for j in range(9):
+                number = self.game.puzzle[i][j]
+                if len(number) == 1:
+                    x = MARGIN + j * SIDE + SIDE // 2
+                    y = MARGIN + i * SIDE + SIDE // 2
+                    self.canvas.create_text(x, y, text=number, tags="numbers", fill="black")
+
+if __name__ == '__main__':
+
+    window = tk.Tk()
+    window.title("Sudoku Solver")
+    window.resizable(width=False, height=False)
+
+    solver = Solver()
+    solver.start()
+
+    ui = Ui(window, solver)
+
+    window.mainloop()
